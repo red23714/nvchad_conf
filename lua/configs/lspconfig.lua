46ba6f815eca8435d2ec7b69e73c6b5bffa4fcbb
red <62202628+red23714@users.noreply.github.com>
2026-06-28 03:18:32 +0300

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "basedpyright", "ts_ls", "tinymist" }
+local servers = { "html", "cssls", "clangd", "basedpyright", "ts_ls", "tinymist", "cssmodules_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
