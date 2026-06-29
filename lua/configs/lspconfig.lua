c500cfc00ce0a3064468fcabf9c020b46ce4a547
red <62202628+red23714@users.noreply.github.com>
2026-06-29 14:40:13 +0300

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,28 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "clangd", "basedpyright", "ts_ls", "tinymist", "cssmodules_ls" }
+
+-- настройка конкретного сервера
+vim.lsp.config("ts_ls", {
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+	settings = {
+		typescript = {
+			preferences = {
+				preferTypeOnlyAutoImport = false,
+			},
+		},
+		javascript = {
+			preferences = {
+				preferTypeOnlyAutoImport = false,
+			},
+		},
+	},
+})
+
+
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
